16390abb50f52a8e54fbe4d1d693243e35d752aa
SayedPython <sayedmahfuze@gmail.com>
2024-01-21 08:35:49 +0530

    Initial Commit for PostgreSQL Database connection

---
+++ PostgreSQLDBServer/db_connection_test.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+# connection test for postgres
+
+import psycopg2
+
+from config import config 
+
+def connect():
+    """ Connect to the PostgreSQL databse server """
+    conn = None 
+
+    try:
+        # read connection parameters 
+        params = config()
+
+        # connect to the PostgreSQL server 
+        print('Connecting to the PostgreSQL databse...')
+
+        conn  = psycopg2.connect(**params)
+
+        # create a cursor 
+        cur = conn.cursor()
+
+        # execute a statement 
+        print('PostgreSQL database version:')
+        cur.execute('SELECT version()')
+
+        # display the PostgreSQL databse server version
+
+        db_version = cur.fetchone()
+        print(db_version)
+
+        # close the communincation with the PostgreSQL 
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Databse connection closed')
+
+
+
+
+    
+if __name__ == '__main__':
+    print("Trying to connect DB...")
+    connect()
+        
